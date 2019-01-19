0a0eab23fa4cf68af08761c3defbf938a250072a
Hein Rutjes <hrutjes@gmail.com>
2019-01-19 14:32:11 +0100

    Added ability to override the `active` state, through `<MagicMove.Scene active={true/false}>`

--- src/index.js
+++ src/index.js
@@ -1,44 +1,5 @@
-import React, { Component } from "react";
-import { Scene } from "react-native-magic-move";
-import { NavigationEvents } from "react-navigation";
+import ReactNavigationScene from './ReactNavigationScene';
 
-class ReactNavigationScene extends Component {
-  state = {
-    active: false,
-    id: undefined
-  };
-
-  render() {
-    const { active, id } = this.state;
-    return (
-      <React.Fragment>
-        <NavigationEvents
-          onWillFocus={this.onWillFocus}
-          onWillBlur={this.onWillBlur}
-          // onDidFocus={payload => console.log("did focus", payload)}
-          // onDidBlur={payload => console.log("did blur", payload)}
-        />
-        {// eslint-disable-next-line
-        React.cloneElement(this.props.children, {
-          active,
-          id: this.props.children.props.id || id // eslint-disable-line
-        })}
-      </React.Fragment>
-    );
-  }
-
-  onWillFocus = event => {
-    this.setState({
-      id: event.state.routeName,
-      active: true
-    });
-  };
-
-  onWillBlur = () => {
-    this.setState({
-      active: false
-    });
-  };
+export {
+    ReactNavigationScene
 }
-
-Scene.addHook(ReactNavigationScene);
